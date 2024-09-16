d8318a587999e9626c5d67f1c061f92e33571563
Md Raihan <rayhan095@gmail.com>
2024-09-16 11:36:08 +0600

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -28,6 +28,9 @@ function install_docker {
     echo "Docker installed successfully!"
     docker --version
 
+    echo "Create docker group"
+    sudo groupadd docker
+
     echo "Adding current user to the docker group..."
     sudo usermod -aG docker $USER
 
